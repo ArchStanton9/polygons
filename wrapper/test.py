8c3ad84df3e360e679de07e4f24de8e122886a43
Koshcheev Nikita <nikitakosheleo@live.ru>
2017-10-29 22:06:29 +0500

    stats

--- wrapper/test.py
+++ wrapper/test.py
@@ -1,16 +1,57 @@
 import build.wrapper.Debug.wrapper as CGAL
+import numpy as np
 import matplotlib.pyplot as plt
 
-random = list(CGAL.random(1000).points())
-halton = list(CGAL.halton(1000).points())
 
-plt.figure(1)
-plt.subplot(211)
-plt.scatter([p.x for p in random], [p.y for p in random], s=1)
-plt.axis([-0.5, 0.5, -0.5, 0.5])
+def compare_point_sets():
+    random = list(CGAL.random(10000).points())
+    halton = list(CGAL.halton(10000).points())
 
-plt.subplot(212)
-plt.scatter([p.x for p in halton], [p.y for p in halton], s=1)
-plt.axis([-0.5, 0.5, -0.5, 0.5])
+    plt.figure(1)
+    plt.subplot(121)
+    plt.scatter([p.x for p in random], [p.y for p in random], s=1)
+    plt.axis([-0.5, 0.5, -0.5, 0.5])
 
-plt.show()
+    plt.subplot(122)
+    plt.scatter([p.x for p in halton], [p.y for p in halton], s=1)
+    plt.axis([-0.5, 0.5, -0.5, 0.5])
+
+    plt.show()
+
+
+def triangulation():
+    ps = list(CGAL.halton(10000).points())
+    x = np.array([p.x for p in ps])
+    y = np.array([p.y for p in ps])
+
+    plt.triplot(x, y)
+    plt.show()
+
+
+def area_histogram():
+    tr = CGAL.Triangulation(CGAL.random(10000))
+    triangles = list(tr.triangles())
+    print(len(triangles))
+    
+    data = np.array([t.area() for t in triangles])
+    plt.hist(data, 100)
+    plt.show()
+
+    
+def angle_histogram():
+    tr = CGAL.Triangulation(CGAL.random(10000))
+    triangles = list(tr.triangles())
+    print(len(triangles))
+
+    data = np.array([t.angle() for t in triangles])
+    plt.hist(data, 60)
+    plt.show()
+    pass
+
+
+if __name__ == '__main__':
+    # compare_point_sets()
+    # triangulation()
+    # area_histogram()
+    # angle_histogram()
+    pass
--- wrapper/wrapper.cpp
+++ wrapper/wrapper.cpp
@@ -2,16 +2,20 @@
 #define BOOST_NUMPY_STATIC_LIB
 
 #include <vector>
+#include <math.h>
 #include <boost/python.hpp>
 #include <halton.hpp>
 
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/point_generators_2.h>
+#include <CGAL/Delaunay_triangulation_2.h>
 
-typedef CGAL::Simple_cartesian<double>              K;
-typedef K::Triangle_2                               Triangle;
-typedef K::Point_2                                  Point;
-typedef CGAL::Random_points_in_square_2<Point>    Generator;
+typedef CGAL::Simple_cartesian<double>          K;
+typedef K::Triangle_2                           Triangle;
+typedef K::Point_2                              Point;
+typedef K::Angle                              Angle;
+typedef CGAL::Delaunay_triangulation_2<K>       Triangulation;
+typedef CGAL::Random_points_in_square_2<Point>  Generator;
 
 using namespace boost::python;
 
@@ -19,6 +23,8 @@ using namespace boost::python;
 
 class PyPoint {
 public:
+    PyPoint() : PyPoint(0.0, 0.0) { }
+
     PyPoint(double x, double y) {
         this->x = x;
         this->y = y;
@@ -28,6 +34,8 @@ public:
         x = p.x();
         y = p.y();
     }
+
+    operator Point () { return Point(x,y); }
 
     double x;
     double y;
@@ -58,6 +66,86 @@ public:
 
 private:
     std::vector<PyPoint> source;
+};
+
+class PyTriangle{
+public:
+    PyTriangle() { }
+    PyTriangle(PyPoint p1, PyPoint p2, PyPoint p3) {
+        a = p1; b = p2; c = p3;
+        source = Triangle(
+            Point(a.x, a.y),
+            Point(b.x, b.y),
+            Point(c.x, c.y));
+    }
+
+    PyTriangle(Point p1, Point p2, Point p3) {
+        a = PyPoint(p1);
+        b = PyPoint(p2);
+        c = PyPoint(p3);
+        source = Triangle(p1, p2, p3);
+    }
+
+    PyPoint a, b, c;
+
+    double area() {
+        return source.area();
+    }
+
+    double angle() {
+        auto a1 = calculate_angle(a, b, c);
+        auto a2 = calculate_angle(b, c, a);
+        auto a3 = calculate_angle(c, a, b);
+
+        return std::min({a1, a2, a3});
+    }
+
+private:
+    Triangle source;
+
+    double calculate_angle(PyPoint p1, PyPoint p2, PyPoint p3) {
+        K::Vector_2 v1(static_cast<Point>(p2), static_cast<Point>(p1));
+        K::Vector_2 v2(static_cast<Point>(p2), static_cast<Point>(p3));
+        auto rad = acos((v1 * v2) / sqrt(v1.squared_length() * v2.squared_length()));
+
+        return rad * 180 / CGAL_PI;
+    }
+};
+
+
+class PyTriangulation
+{
+public:
+    PyTriangulation(PointSet points) {
+        std::stringstream st;
+        for (auto p : points) {
+            st << p.x << " " << p.y << std::endl;
+        }
+
+        std::istream_iterator<Point> begin(st);
+        std::istream_iterator<Point> end;
+        Triangulation t;
+        t.insert(begin, end);
+
+        for (auto f = t.faces_begin(); f < t.faces_end(); f++) {
+            auto p1 = f->vertex(0)->point();
+            auto p2 = f->vertex(1)->point();
+            auto p3 = f->vertex(2)->point();
+
+            triangles.emplace_back(PyTriangle(p1, p2, p3));
+        }
+    }
+
+    std::vector<PyTriangle>::iterator begin() {
+        return triangles.begin();
+    }
+
+    std::vector<PyTriangle>::iterator end() {
+        return triangles.end();
+    }
+
+private:
+    std::vector<PyTriangle> triangles;
 };
 
 #pragma endregion
@@ -92,13 +180,24 @@ char const* yay() {
 }
 
 BOOST_PYTHON_MODULE(wrapper) {
-    class_<PointSet>("PointSet")
-        .def("points", range(&PointSet::begin, &PointSet::end));
-
     class_<PyPoint, boost::shared_ptr<PyPoint>>(
         "Point", init<double, double>())
         .def_readwrite("x", &PyPoint::x)
         .def_readwrite("y", &PyPoint::y);
+
+    class_<PyTriangle, boost::shared_ptr<PyTriangle>>(
+        "Triangle", init<PyPoint, PyPoint, PyPoint>())
+        .def_readwrite("a", &PyTriangle::a)
+        .def_readwrite("b", &PyTriangle::b)
+        .def_readwrite("c", &PyTriangle::c)
+        .def("area", &PyTriangle::area)
+        .def("angle", &PyTriangle::angle);
+
+    class_<PointSet>("PointSet")
+        .def("points", range(&PointSet::begin, &PointSet::end));
+
+    class_<PyTriangulation>("Triangulation", init<PointSet>())
+        .def("triangles", range(&PyTriangulation::begin, &PyTriangulation::end));
 
     def("random", GeneratePoints);
     def("halton", GenerateHalton);
